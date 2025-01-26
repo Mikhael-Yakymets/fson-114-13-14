c6d1a7cceafd5aa80def4e34fba9db8985ab6990
KaterynaSiv <kgusieva@gmail.com>
2025-01-26 19:57:17 +0100

    task 6

--- js/lessons2.js
+++ js/lessons2.js
@@ -143,3 +143,35 @@
 // }
 // console.log(discountFructis(fruits));
 // console.log(fruits);
+
+// Угруповання об'єктів за якістю
+// Створіть масив об'єктів, що являють собою різних студентів. Кожен об'єкт повинен містити властивості ім'я, вік та курс. Використовуйте цикл for...of, щоб згрупувати студентів за курсами.
+// Результатом буде об'єкт, де ключами будуть назви курсів, а значення – масиви об'єктів студентів, що належать до відповідного курсу.
+// Очікуванний результат:
+// const groupedStudents = {
+//   Java: [
+//     { name: "Аліса", age: 20, course: "Java" },
+//     { name: "Карл", age: 21, course: "Java" },
+//   ],
+//   Python: [{ name: "Боб", age: 22, course: "Python" }],
+//   JavaScript: [{ name: "Джон", age: 23, course: "JavaScript" }],
+// };
+
+const students = [
+  { name: 'Аліса', age: 20, course: 'Java' },
+  { name: 'Боб', age: 22, course: 'Python' },
+  { name: 'Карл', age: 21, course: 'Java' },
+  { name: 'Джон', age: 23, course: 'JavaScript' },
+];
+
+const groupedStudents = {};
+
+for (const student of students) {
+  if (!groupedStudents[student.course]) {
+    groupedStudents[student.course] = [];
+  }
+  groupedStudents[student.course].push(student);
+}
+console.log(groupedStudents);
+// const id = 5;
+// groupedStudents[id] = 'aa';
