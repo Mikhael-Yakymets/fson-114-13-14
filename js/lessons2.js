b9b40a130e418b66a3fe280cc11dc3d60291df2b
Mykhailo Yakymets <vskitano@gmail.com>
2025-01-26 19:08:32 +0200

    task 2.4

--- js/lessons2.js
+++ js/lessons2.js
@@ -75,3 +75,28 @@
 // }
 // console.log(calcTotalPrice(stones, 'Діамант'));
 // console.log(calcTotalPrice(stones, 'Діаман'));
+
+//Task 4
+// Створіть об'єкт calculator із трьома методами
+// read(a, b) - приймає два аргументи та зберігає їх
+// як властивості об'єкта
+// sum() повертає суму збережених значень
+// multiply() перемножує збережені значення та повертає результат
+
+const calculator = {
+  read(a, b) {
+    this.val1 = a;
+    this.val2 = b;
+  },
+  sum() {
+    return this.val1 + this.val2;
+  },
+  multiply() {
+    return this.val1 * this.val2;
+  },
+};
+
+calculator.read(12, 22);
+console.log(calculator);
+console.log(calculator.sum());
+console.log(calculator.multiply());
